56ca1f623ea53d99fe7e34b81cc0fe8aa88dab35
jarpraca <jarpraca@gmail.com>
2020-01-02 23:34:32 +0000

    Added another version, still not working

--- Trabalho2/gap.pl
+++ Trabalho2/gap.pl
@@ -8,15 +8,68 @@ getCell(Board, Row, Col, Cell) :-
     nth1(Row, Board, Line),
     nth1(Col, Line, Cell).
 
+checkAllCells(Board) :-
+    checkAllLines(Board, Board, 1).
+
+checkAllLines(_Board, [], _Row).
+
+checkAllLines(Board, [Line | Lines], Row) :-
+    checkLine(Board, Line, Row, 1),
+    NextRow is Row+1,
+    checkAllLines(Board, Lines, NextRow).
+
+checkLine(_Board, [], _Row, _Col).
+
+checkLine(Board, [E | Line], Row, Col) :-
+    %format("~w ~w ~w", [Row, Col, E]),nl,
+    checkSurroundingCells(Board, E, Row, Col),
+    NextCol is Col+1,
+    checkLine(Board, Line, Row, NextCol).
+
+% checkAllCells(Board, Row, Col)
+
+% checkAllCells(Board, Row, Col) :-
+%     getCell(Board, Row, Col, Cell),
+%     checkSurroundingCells(Board, Cell, Row, Col),
+    
+
 /**
 * Checks if a cell located in Row and Col is black and if its surroundings cells are all white
 */
-checkSurroundingCells(Board, Cell) :-
-    getCell(Board, Row, Col, Cell),
+
+
+% checkSurroundingCells(_Board, Vars, 0) :-
+%     getCell(Board, Row, Col, Cell),
+%     format("~w ~w ~w", [Row, Col, Cell]),nl,
+%     print(Vars),nl.
+
+
+checkSurroundingCells(Board, Cell, Row, Col) :-
+    % getCell(Board, Row, Col, Cell),
     %nth1(Row, Board, Line),
-    %format("~w ~w", [Row, Line]),nl,
+    %format("~w ~w ~w", [Row, Col, 1]),nl,
     %Cell #= 1, 
+    ground(Cell),
+    Cell == 1,
+    print('check'),nl,
     checkSurroundingCellsAux(Board, Row, Col).
+
+checkSurroundingCells(Board, Cell, Row, Col) :-
+    % getCell(Board, Row, Col, Cell),
+    %nth1(Row, Board, Line),
+    %format("~w ~w ~w", [Row, Col, 1]),nl,
+    %Cell #= 1, 
+    ground(Cell),
+    Cell == 0.
+
+checkSurroundingCells(Board, Cell, Row, Col) :-
+    (  
+        Cell==1 ->
+        checkSurroundingCellsAux(Board, Row, Col);
+        true
+    ).
+
+%checkSurroundingCells(_Board, _Cell, _Row, _Col).
 
 checkSurroundingCellsAux(Board, 1, 1) :-
     Cell4Row is 1+1,
@@ -233,7 +286,14 @@ countSpacesAux([0 | List], N, Count) :-
     NextN is N+1,
     countSpacesAux(List, NextN, Count).
 
-play(Vars) :-
+printLines([]).
+
+printLines([Line | Board]) :-
+    print(Line),
+    nl,
+    printLines(Board).
+
+ant(Lines) :-
     Lines=[ [C11,C12,C13,C14,C15,C16,C17,C18,C19],
             [C21,C22,C23,C24,C25,C26,C27,C28,C29],
             [C31,C32,C33,C34,C35,C36,C37,C38,C39],
@@ -250,14 +310,184 @@ play(Vars) :-
     same_sum(Lines, Sum),
     same_sum(Columns, Sum),
     Sum #= 2,
-    maplist(checkSurroundingCells(Lines), Vars),nl,
-    %write('HELLO'),nl,
     nth1(2, Columns, Restr1),
-    %print(Restr1),nl,
     countSpaces(Restr1, Count1),
-    %print(Count1),nl,
     Count1 #= 5,
     nth1(8, Lines, Restr2),
     countSpaces(Restr2, Count2),
     Count2 #= 4,
+    print('BEFORE'),nl,
+    printLines(Lines),nl,
+    %maplist(checkSurroundingCells(Lines), Vars),
+    %printLines(Lines),nl,
+    checkAllCells(Lines),
+    print('AFTER'),nl,
+    printLines(Lines),nl,
+    labeling([], Vars),
+    printLines(Lines).
+
+%-----------------------------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------
+
+flat_list(Cols, Vars) :-
+    flat_list(Cols, [], Vars).
+
+flat_list([], Aux, Aux).
+
+flat_list([C | Cols], Aux, Vars) :-
+    append(C, NewC),
+    append(Aux, NewC, NewAux),
+    flat_list(Cols, NewAux, Vars).
+
+getRow([Row, _Col], Row).
+
+getCol([_Row, Col], Col).
+
+getAllRows(Coord, Rows) :-
+    getAllRows(Coord, [], Rows).
+
+getAllRows([], Aux, Aux).
+
+getAllRows([E | Coord], Aux, Rows) :-
+    getRow(E, Row),
+    append(Aux, [Row], NextAux),
+    getAllRows(Coord, NextAux, Rows).
+
+countRows(Coord) :-
+    countRows(Coord, 9).
+
+countRows(Coord, 0).
+
+countRows(Coord, N) :-
+    getAllRows(Coord, Rows),
+    global_cardinality(Rows, [1-2,2-2,3-2,4-2,5-2,6-2,7-2,8-2,9-2]),
+    Next is N-1,
+    countRows(Coord, Next).
+
+
+getAllCols(Coord, Cols) :-
+    getAllCols(Coord, [], Cols).
+
+getAllCols([], Aux, Aux).
+
+getAllCols([E | Coord], Aux, Cols) :-
+    getCol(E, Col),
+    append(Aux, [Col], NextAux),
+    getAllCols(Coord, NextAux, Cols).
+
+countCols(Coord) :-
+    countCols(Coord, 9).
+
+countCols(Coord, 0).
+
+countCols(Coord, N) :-
+    getAllCols(Coord, Cols),
+    global_cardinality(Cols, [1-2,2-2,3-2,4-2,5-2,6-2,7-2,8-2,9-2]),
+    Next is N-1,
+    countCols(Coord, Next).
+
+getPieceByRow(Coord, Row, Piece) :-
+    nth1(I, Coord, [Row, _Col]),
+    nth1(I, Coord, Piece).
+
+diff(T2, 0, T2).
+
+diff(T2, T1, Diff) :-
+    NextT2 is T2-1,
+    NextT1 is T1-1,
+    diff(NextT2, NextT1, Diff).
+
+checkSpacesInRow(Coord, Row, Spaces) :-
+    findall(Piece, getPieceByRow(Coord, Row, Piece), Pieces),
+    nth1(1, Pieces, Piece1),
+    nth1(2, Pieces, Piece2),
+    nth1(2, Piece1, Piece1Col),
+    nth1(2, Piece2, Piece2Col),
+    % List = [Piece1Col, Piece2Col],
+    % sort(List, ListOrdered),
+    nth1(1, ListOrdered, Min),
+    nth1(2, ListOrdered, Max),
+    %diff(Max, Min, Diff),
+    ((Max-Min-1) #= Spaces;
+    (Min-Max+1) #= -Spaces).
+
+getPieceByCol(Coord, Col, Piece) :-
+    nth1(I, Coord, [_Row, Col]),
+    nth1(I, Coord, Piece).
+
+checkSpacesInCol(Coord, Col, Spaces) :-
+    findall(Piece, getPieceByCol(Coord, Col, Piece), Pieces),
+    nth1(1, Pieces, Piece1),
+    nth1(2, Pieces, Piece2),
+    nth1(1, Piece1, Piece1Row),
+    nth1(1, Piece2, Piece2Row),
+    %diff(Max, Min, Diff),
+    (abs(Piece2Row-Piece1Row)-1) #= Spaces.
+
+surround([]).
+surround([E | Coord]) :-
+    alone(E, Coord),
+    surround(Coord).
+
+alone(_E1, []).
+alone(E1, [E2 | Coord]) :-
+    notNear(E1, E2),
+    alone(E1, Coord).
+
+notNear([R1, C1], [R2, C2]) :-
+    abs(R2-R1) #> 1;
+    abs(C2-C1) #> 1.
+
+notRepeated([]).
+
+notRepeated([E | Coord]) :-
+    checkNotRepeated(E, Coord),
+    notRepeated(Coord).
+
+checkNotRepeated(_E1, []).
+
+checkNotRepeated(E1, [E2 | Coord]) :-
+    checkPair(E1, E2),
+    checkNotRepeated(E1, Coord).
+
+checkPair([R1, C1], [R2, C2]) :-
+    R1 #\= R2;
+    C1 #\= C2.
+
+play(Coord) :-
+    Coord=[ [R1,C1],
+            [R2,C2],
+            [R3,C3],
+            [R4,C4],
+            [R5,C5],
+            [R6,C6],
+            [R7,C7],
+            [R8,C8],
+            [R9,C9],
+            [R10,C10],
+            [R11,C11],
+            [R12,C12],
+            [R13,C13],
+            [R14,C14],
+            [R15,C15],
+            [R16,C16],
+            [R17,C17],
+            [R18,C18]],
+    append(Coord, Vars),
+    domain(Vars, 1, 9),
+    % constraints
+    notRepeated(Coord),
+    countRows(Coord),
+    print('countRows'),nl,
+    countCols(Coord),
+    print('countCols'),nl,
+    print(Coord),nl,
+    surround(Coord),
+    print('surround'),nl,
+    print(Coord),nl,
+    checkSpacesInRow(Coord, 8, 4),
+    checkSpacesInCol(Coord, 2, 5),
+    %print('checkSpaces'),nl,
     labeling([], Vars).
